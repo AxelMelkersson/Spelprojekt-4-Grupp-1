9b1acab0aca2d9c7cf2776e437f0a50ae15b0a0e
AxelMelkersson <axel.melkersson@studerande.thegameassembly.se>
2021-04-08 13:19:39 +0200

    Fixed Z-index once again :(

--- Source/Game/InputWrapper.cpp
+++ Source/Game/InputWrapper.cpp
@@ -72,7 +72,7 @@ v2f InputWrapper::GetAxisMovement()
 
 bool InputWrapper::IsMovingUp()
 {
-	if (myInput->GetKeyDown(Keys::WKey) || GetController()->GetLeftThumbStick().y < -0.9f || GetController()->IsButtonHoldDown(Controller::Button::DPadUp))
+	if (myInput->GetKeyDown(Keys::WKey) || GetController()->GetLeftThumbStick().y < 0.0f || GetController()->IsButtonHoldDown(Controller::Button::DPadUp))
 		return true;
 	else
 		return false;
