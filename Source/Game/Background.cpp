8e7bc5ebef717516d4376c3cb1ccd62f8f25e946
Daniel <71003000+Dodjoj@users.noreply.github.com>
2021-04-13 18:07:53 +0200

    Two backgrounds after each other

--- Source/Game/Background.cpp
+++ Source/Game/Background.cpp
@@ -201,6 +201,9 @@ const void Background::CreateBackgrounds(Scene* aLevelScene, const std::string a
 		myBackgroundSprite5 = new GameObject(aLevelScene);
 		SpriteComponent* sprite = myBackgroundSprite5->AddComponent<SpriteComponent>();
 		sprite->SetSpritePath(aPath);
+		SpriteComponent* sprite2 = myBackgroundSprite5->AddComponent<SpriteComponent>();
+		sprite2->SetSpritePath(aPath);
+		sprite2->SetRelativePosition({ sprite->GetImageSize().x, 0.f });
 		myBackgroundSprite5->SetZIndex(4);
 		myOffsetBackground5 = anOffset;
 		break;
@@ -210,7 +213,7 @@ const void Background::CreateBackgrounds(Scene* aLevelScene, const std::string a
 		myBackgroundSprite6 = new GameObject(aLevelScene);
 		SpriteComponent* sprite = myBackgroundSprite6->AddComponent<SpriteComponent>();
 		sprite->SetSpritePath(aPath);
-		SpriteComponent* sprite2 = myBackgroundSprite2->AddComponent<SpriteComponent>();
+		SpriteComponent* sprite2 = myBackgroundSprite6->AddComponent<SpriteComponent>();
 		sprite2->SetSpritePath(aPath);
 		sprite2->SetRelativePosition({ sprite->GetImageSize().x, 0.f });
 		myBackgroundSprite6->SetZIndex(5);
@@ -222,7 +225,7 @@ const void Background::CreateBackgrounds(Scene* aLevelScene, const std::string a
 		myBackgroundSprite7 = new GameObject(aLevelScene);
 		SpriteComponent* sprite = myBackgroundSprite7->AddComponent<SpriteComponent>();
 		sprite->SetSpritePath(aPath);
-		SpriteComponent* sprite2 = myBackgroundSprite2->AddComponent<SpriteComponent>();
+		SpriteComponent* sprite2 = myBackgroundSprite7->AddComponent<SpriteComponent>();
 		sprite2->SetSpritePath(aPath);
 		sprite2->SetRelativePosition({ sprite->GetImageSize().x, 0.f });
 		myBackgroundSprite7->SetZIndex(6);
@@ -288,8 +291,12 @@ const void Background::CalculateCameraPositions(const float& aDeltaTime)
 	myBackgroundSprite4->SetPosition(cameraPosition + backgroundSpeedFour + GetHalfImageSize(myBackgroundSprite4) + myOffsetBackground4);
 	myBackgroundSprite5->SetPosition(cameraPosition + backgroundSpeedFive + GetHalfImageSize(myBackgroundSprite5) + myOffsetBackground5);
 	myBackgroundSprite6->SetPosition(cameraPosition + backgroundSpeedSix + GetHalfImageSize(myBackgroundSprite6) + myOffsetBackground6);
-	//myBackgroundSprite7->SetPosition(cameraPosition + backgroundSpeedSeven + GetHalfImageSize(myBackgroundSprite7) + myOffsetBackground7);
-	myBackgroundSprite7->SetPosition({10.f * aDeltaTime, 0.f});
+	myBackgroundSprite7->SetPosition(cameraPosition + backgroundSpeedSeven + GetHalfImageSize(myBackgroundSprite7) + myOffsetBackground7);
+	//myBackgroundSprite5->SetPosition({ myBackgroundSprite5->GetPositionX() + -40.f * aDeltaTime, GetHalfImageSize(myBackgroundSprite5).y });
+	//myBackgroundSprite6->SetPosition({ myBackgroundSprite6->GetPositionX() + -40.f * aDeltaTime, GetHalfImageSize(myBackgroundSprite6).y });
+	//myBackgroundSprite7->SetPosition({ myBackgroundSprite7->GetPositionX() + -40.f * aDeltaTime, GetHalfImageSize(myBackgroundSprite7).y });
+
+
 
 	*myCloudDistance = *myCloudDistance + (aDeltaTime * myCloudSpeed);
 
@@ -305,6 +312,11 @@ const void Background::CalculateCameraPositions(const float& aDeltaTime)
 const void Background::ResetDistanceValues()
 {
 	Distance::myBackgroundDistanceX = {};
+}
+
+const void Background::CheckResetLoop(GameObject* aGameObject)
+{
+
 }
 
 
